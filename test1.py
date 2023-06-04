1a46423524bc3600e81a471a46cc2572cbf84bef
bikram <bikramshah213@gmail.com>
2023-06-04 13:11:03 +0545

    this is my first push in git and did it

--- test1.py
+++ test1.py
@@ -1,16 +1,48 @@
 import pymongo
 client=pymongo.MongoClient('mongodb+srv://root:root@cluster0.cr61eot.mongodb.net/?retryWrites=true&w=majority')
+# client=pymongo.MongoClient('mongodb+srv://root:root@cluster0.cr61eot.mongodb.net/')
+
 db=client.test
 print(db)
 
 d={
-    'name':'hari',
-    'sirname':'jha',
-    'crn':'PUR078MSInE003'
-
+    'fname':'bikram',
+    'lname':'shah',
+    'faculty':'computer'
 }
-print("this is the changed code")
 
-db1=client['college']
-coll=db1['student']
-coll.insert_one(d)
+
+datas=[
+    {
+        'name':'hari',
+        'class':10,
+        'subject':['C','C++','Java']
+    },
+    {
+        'name':'manoj',
+        'class':9,
+        'subject':['ECT','C++','Java']
+    },
+    {
+        'name':'monu',
+        'class':9,
+        'subject':['ECT','EDC','Java']
+    }
+
+
+]
+# print(d)
+#
+# mydb=client['mydatabasename']
+# std=mydb['student']
+# std.insert_one(d)
+# print("data inserted")
+# print(datas)
+database=client['mydatabase']
+collection=database['student']
+# collection.insert_one(d)
+# collection.insert_many(datas)
+records=collection.find()
+for i in records:
+    for j in i.items():
+        print(j[1])
